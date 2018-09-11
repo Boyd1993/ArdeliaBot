2630909fddc3c85db0d91b97778dc180f02c6273
Boyd1993 <B.C.Verdoorn@student.tudelft.nl>
2018-09-11 14:03:57 +0200

    Getting ready for deployment to a vps

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
+require('dotenv').config()
 require('http').createServer().listen(3000)
 
 const client = new Discord.Client();
