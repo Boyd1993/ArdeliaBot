6021e6670e74b7273a107d64a442359c2444890f
Boyd1993 <B.C.Verdoorn@student.tudelft.nl>
2018-09-12 04:35:09 +0200

    hotfix

--- commands/setgameofbot.js
+++ commands/setgameofbot.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args, guildConfig) => {
   if(message.member.user.id === '348822302458380288'){
     client.user.setPresence({ game: { name: args.join(' ') }, status: 'online' })
-    .then(send.channel.message("Status is updated!"))
+    .then(message.channel.send("Status is updated!"))
     .catch(console.error);
 
   }
