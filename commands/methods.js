607b8aaaf67c7d40061012c7c39f3480b6186e61
Boyd1993 <B.C.Verdoorn@student.tudelft.nl>
2018-09-14 17:46:29 +0200

    Username to displayName

--- commands/methods.js
+++ commands/methods.js
@@ -119,11 +119,11 @@ module.exports = function(){
       argsArray.forEach(word => {
         if(word === '$(1)'){
 
-          outputString=outputString + "**" + messageObj.author.username + "** ";
+          outputString=outputString + "**" + messageObj.author.displayName + "** ";
           return;
         }
         if(word === '$(2)' && messageObj.mentions.members.size !== 0){
-          outputString = outputString +"**" + messageObj.mentions.members.first().user.username + "** ";
+          outputString = outputString +"**" + messageObj.mentions.members.first().user.displayName + "** ";
           return;
         }
         if (word === '$(2)' && messageObj.mentions.members.size === 0) {
