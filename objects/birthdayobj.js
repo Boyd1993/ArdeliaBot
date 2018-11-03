e629a826f2d3663840d0e6b9f2f4e240fa756266
Boyd1993 <B.C.Verdoorn@student.tudelft.nl>
2018-11-03 15:36:32 +0100

    Update objects/birthdayobj.js

--- objects/birthdayobj.js
+++ objects/birthdayobj.js
@@ -16,7 +16,6 @@ function displayDate(){
   }
   else{
     let currentDate = new Date();
-    console.log(currentDate.getUTCMonth());
     if ((currentDate.getUTCMonth()+1 == this.month && currentDate.getUTCDate() >= this.day) || currentDate.getUTCMonth()+1 > this.month){
       let date = this.day +"-"+ this.month + "-" + this.year + "--> Did become " + (currentDate.getUTCFullYear() - this.year) + " years old";
       return date;
