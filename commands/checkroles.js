5997d9023fa5a3074f53d1aa29607e354d8be678
Boyd1993 <B.C.Verdoorn@student.tudelft.nl>
2018-11-22 11:41:42 +0100

    Update checkroles.js

--- commands/checkroles.js
+++ commands/checkroles.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args, guildConfig) => {
   if(isModerator(message.member,guildConfig)){
     const fs = require("fs");
     require('./methods.js')();
-    let roleToChange = args.slice(1, args.length);
+    let roleToChange = args.slice(1, args.length).join(' ');
 
     if (args[0] === 'addModerator'){
       if(checkRoleExist(roleToChange,getRoleNames(message.guild.roles))){
