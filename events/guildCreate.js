fd1fedbce883a3124e90664a9edc8f18f0b857b5
Boyd1993 <B.C.Verdoorn@student.tudelft.nl>
2018-11-22 10:20:43 +0100

    Guildcreate hotfix

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,6 @@
 module.exports = (client, guild) => {
   const fs = require("fs");
-  const path = require('paths');
+  const path = require('path');
   require(path.join(__dirname,'..','commands', 'methods.js'))();
   writeNewSaveFile(guild.id,fs);
 
