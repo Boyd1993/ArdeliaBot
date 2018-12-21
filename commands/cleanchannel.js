eed1b89ac7414278412f5b473e211f2cc6df7b4b
Boyd1993 <B.C.Verdoorn@student.tudelft.nl>
2018-12-21 20:11:24 +0100

    Update cleanchannel.js

--- commands/cleanchannel.js
+++ commands/cleanchannel.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args, guildConfig) => {
     if(message.channel.id == 525229064064073738){
       message.channel.fetchMessages()
       .then(messages =>{
-        message.channel.bulkDelete(messages.size - 3)
+        message.channel.bulkDelete(messages.size - 2)
         .then()
         .catch(console.error);
       })
