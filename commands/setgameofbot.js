981393a04abc9c53804cb91aafccba0fa9741248
bcv93 <boydverdoorn@msn.com>
2019-09-29 23:13:01 +0200

    Made a ready event so game is set on start of client.

--- commands/setgameofbot.js
+++
@@ -1,8 +0,0 @@
-exports.run = (client, message, args, guildConfig) => {
-  if(message.member.user.id === '348822302458380288'){
-    client.user.setPresence({ game: { name:  'https://twitter.com/Felanoraa' }, status: 'online' })
-    .then(message.channel.send("Status is updated!"))
-    .catch(console.error);
-
-  }
-}
