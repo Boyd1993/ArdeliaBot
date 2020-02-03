8a734b3c7e9ab32d6ac52707863ea5d83e9e998b
bcv93 <boydverdoorn@msn.com>
2020-02-03 20:31:35 +0100

    new logger functionality

---
+++ util/logger.js
@@ -0,0 +1,28 @@
+module.exports.error = function (logMessage, guild) {
+
+    const path = require('path');
+    const fs = require('fs');
+    const logFilePath = path.join(__dirname, "..", "logs", `${getDateNowString()}.txt`);
+
+    try {
+        fs.appendFile(logFilePath, formatMessage(), (err) => {
+            console.error(err);
+        });
+    }
+    catch (e) {
+        console.error(e);
+    }
+
+    function getDateNowString() {
+        return new Date().toISOString()
+            .replace(/\T.+/, '');
+    }
+
+    function formatMessage() {
+        var dateString = new Date().toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, '');
+        return `${dateString}\t${guild.id}\t${guild.name}\tERROR:\t${logMessage}\n`
+    }
+}
+
